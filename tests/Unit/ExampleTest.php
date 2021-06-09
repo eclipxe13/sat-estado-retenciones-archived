e083e7fe4a04116bc23256d195135a1257c344c8
Carlos C Soto <eclipxe13@gmail.com>
2021-06-08 20:49:53 -0500

    Update config to php-cs-fixer:^3.0

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -9,7 +9,7 @@ use PhpCfdi\SatEstadoRetenciones\Tests\TestCase;
 
 final class ExampleTest extends TestCase
 {
-    public function testAssertIsworking()
+    public function testAssertIsworking(): void
     {
         $example = new Example();
         $this->assertInstanceOf(Example::class, $example);
