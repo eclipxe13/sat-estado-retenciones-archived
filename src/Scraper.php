4e6495f71161e8c152229ec61ee6e6500ce9d332
Carlos C Soto <eclipxe13@gmail.com>
2021-06-09 08:58:30 -0500

    Extract method makeUrl

--- src/Scraper.php
+++ src/Scraper.php
@@ -17,19 +17,19 @@ class Scraper
 
     public function obtainStatus(RetentionQuery $query): RetentionResult
     {
-        // build url
-        $url = self::SAT_WEBAPP_URL . '?' . http_build_query([
+        $url = $this->makeUrl($query);
+        $html = file_get_contents($url);
+        return $this->resultConverter->convertHtml($html);
+    }
+
+    public function makeUrl(RetentionQuery $query): string
+    {
+        return self::SAT_WEBAPP_URL . '?' . http_build_query([
             'folio' => $query->getUuid(),
             'rfcEmisor' => $query->getIssuerRfc(),
             'rfcReceptor' => $query->getReceiverRfc(),
             '_' => $this->obtainMillisecondsParameter(),
         ]);
-
-        // get contents from url
-        $html = file_get_contents($url);
-
-        // convert html contents to result
-        return $this->resultConverter->convertHtml($html);
     }
 
     public function obtainMillisecondsParameter(float $microtime = null): int
--- tests/Unit/ScraperTest.php
+++ tests/Unit/ScraperTest.php
@@ -4,11 +4,31 @@ declare(strict_types=1);
 
 namespace PhpCfdi\SatEstadoRetenciones\Tests\Unit;
 
+use PhpCfdi\SatEstadoRetenciones\RetentionQuery;
 use PhpCfdi\SatEstadoRetenciones\Scraper;
 use PhpCfdi\SatEstadoRetenciones\Tests\TestCase;
 
 final class ScraperTest extends TestCase
 {
+    public function testMakeUrlHasValuesOnQueryString(): void
+    {
+        $query = new RetentionQuery('12345678-1234-1234-1234-123456789012', 'AAA010101AAA', 'XXXX991231XX0');
+        $scraper = new Scraper();
+        $url = $scraper->makeUrl($query);
+        $queryString = parse_url($url, PHP_URL_QUERY);
+        parse_str($queryString, $queryValues);
+
+        $expectedValues = array_filter([
+            'folio' => '12345678-1234-1234-1234-123456789012',
+            'rfcEmisor' => 'AAA010101AAA',
+            'rfcReceptor' => 'XXXX991231XX0',
+            '_' => $queryValues['_'] ?? null
+        ]);
+
+        $this->assertSame($expectedValues, $queryValues);
+        $this->assertTrue(is_numeric($queryValues['_'] ?? null), 'url does not contains "_" on query string');
+    }
+
     public function testObtainMillisecondsParameter(): void
     {
         $microtime = idate('U', strtotime('2021-01-13T14:15:16 -06:00')) + 0.123123;
