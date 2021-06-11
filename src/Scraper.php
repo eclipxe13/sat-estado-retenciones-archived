b1f316aa950161ec15df33e8216fa5d22c6693bf
Carlos C Soto <eclipxe13@gmail.com>
2021-06-10 22:43:25 -0500

    Improve obtainStatus phpdoc

--- src/Scraper.php
+++ src/Scraper.php
@@ -27,12 +27,12 @@ class Scraper
     }
 
     /**
-     * Consumes the web page to obtain the information about a document CFDI Retentions
+     * Consumes the web page to obtain the information about a CFDI Retentions document
      *
      * @param Parameters $parameters
      * @return Result
-     * @throws Exceptions\RetentionNotFoundException when unable retention document was not found
-     * @throws Exceptions\HttpClientException when unable to retrieve contents
+     * @throws Exceptions\RetentionNotFoundException if retention document was not found
+     * @throws Exceptions\HttpClientException if unable to retrieve contents because HTTP error
      */
     public function obtainStatus(Parameters $parameters): Result
     {
