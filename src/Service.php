c2de136588cea3115dce48d477bd6051b106dc54
Carlos C Soto <eclipxe13@gmail.com>
2021-06-11 22:13:51 -0500

    Add Service class as the library entry point

---
+++ src/Service.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpCfdi\SatEstadoRetenciones;
+
+use DOMDocument;
+use PhpCfdi\SatEstadoRetenciones\Internal\RetentionReader;
+
+final class Service
+{
+    private Scraper $scraper;
+
+    public function __construct(Scraper $scraper = null)
+    {
+        $this->scraper = $scraper ?? new Scraper();
+    }
+
+    /**
+     * Query parameters and obtain the result
+     *
+     * @param Parameters $parameters
+     * @return Result
+     * @throws Exceptions\RetentionNotFoundException if retention document was not found
+     * @throws Exceptions\HttpClientException if unable to retrieve contents because HTTP error
+     */
+    public function query(Parameters $parameters): Result
+    {
+        return $this->scraper->obtainStatus($parameters);
+    }
+
+    /**
+     * Query parameters and obtain the result, if not found returns NULL
+     *
+     * @param Parameters $parameters
+     * @return Result|null
+     * @throws Exceptions\HttpClientException if unable to retrieve contents because HTTP error
+     */
+    public function queryOrNull(Parameters $parameters): ?Result
+    {
+        try {
+            return $this->scraper->obtainStatus($parameters);
+        } catch (Exceptions\RetentionNotFoundException $exception) {
+            return null;
+        }
+    }
+
+    /**
+     * Makes a parameters object for the given XML
+     *
+     * @param string $xml
+     * @return Parameters
+     */
+    public function makeParametersFromXml(string $xml): Parameters
+    {
+        $document = new DOMDocument();
+        $document->loadXML($xml);
+        return $this->makeParametersFromDocument($document);
+    }
+
+    /**
+     * Makes a parameters object for the given DOM Document
+     *
+     * @param DOMDocument $document
+     * @return Parameters
+     */
+    public function makeParametersFromDocument(DOMDocument $document): Parameters
+    {
+        $reader = new RetentionReader($document);
+
+        return new Parameters(
+            $reader->obtainUUID(),
+            $reader->obtainRfcIssuer(),
+            $reader->obtainRfcReceiver(),
+        );
+    }
+}
