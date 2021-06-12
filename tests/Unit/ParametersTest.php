297ea70fad72831519b11bb3e4d0f74c17fc0a91
Carlos C Soto <eclipxe13@gmail.com>
2021-06-11 23:07:20 -0500

    Move ParametersTest and ResultTest to correct location

--- tests/Unit/ParametersTest.php
+++ tests/Unit/ParametersTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace PhpCfdi\SatEstadoRetenciones\Tests\Unit\ValueObjects;
+namespace PhpCfdi\SatEstadoRetenciones\Tests\Unit;
 
 use JsonSerializable;
 use PhpCfdi\SatEstadoRetenciones\Parameters;
